555d6a10361553f96dc8d2014d1b3731bba6e45a
Ansat Jartybayev <ansatjr@gmail.com>
2021-12-02 23:59:26 +0600

    feat: routing

---
+++ titanium/src/shared/lib/routing/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {useSelector} from "react-redux";
+import {Route, RouteProps, Navigate} from "react-router-dom";
+
+import {authSelectors} from "@features/auth";
+
+interface PrivateRouteProps extends RouteProps {}
+
+export const PrivateRoute: React.FC<PrivateRouteProps> = (props) => {
+  const isAuthenticated = useSelector(authSelectors.isAuthenticated);
+
+  if (!isAuthenticated) return <Navigate to="/" />;
+
+  return <Route {...props} />;
+};
+
+interface PublicOnlyRouteProps extends RouteProps {}
+
+export const PublicOnlyRoute: React.FC<PublicOnlyRouteProps> = (props) => {
+  const isAuthenticated = useSelector(authSelectors.isAuthenticated);
+
+  if (isAuthenticated) return <Navigate to="/" />;
+
+  return <Route {...props} />;
+};
