55a786aa06bcc4029771e4d4dbcc6dc16ed93738
Ansat Jartybayev <ansatjr@gmail.com>
2021-12-04 12:21:56 +0600

    feat: auth-api

--- titanium/src/shared/api/auth.ts
+++ titanium/src/shared/api/auth.ts
@@ -1,3 +1,7 @@
+import {AxiosPromise} from "axios";
+
+import {request} from "./request";
+
 export interface Credentials {
   id: string;
   username: string;
@@ -18,3 +22,53 @@ export interface Credentials {
     isCalibrated: boolean;
   };
 }
+
+interface LoginData {
+  username: string;
+  password: string;
+}
+
+interface LoginResponse {
+  credentials: Credentials;
+}
+
+const login = (data: LoginData): AxiosPromise<LoginResponse> =>
+  request({
+    url: "/api/auth/login",
+    method: "POST",
+    data,
+  });
+
+interface RegisterData {
+  username: string;
+  password: string;
+}
+
+interface RegisterResponse {
+  credentials: Credentials;
+}
+
+const register = (data: RegisterData): AxiosPromise<RegisterResponse> =>
+  request({
+    url: "/api/auth/register",
+    method: "POST",
+    data,
+  });
+
+interface GetCredentialsResponse {
+  credentials: Credentials;
+}
+
+const getCredentials = (): AxiosPromise<GetCredentialsResponse> =>
+  request({
+    url: "/api/auth/credentials",
+    method: "GET",
+  });
+
+const logout = () =>
+  request({
+    url: "/api/auth/logout",
+    method: "POST",
+  });
+
+export const authApi = {login, register, getCredentials, logout};
