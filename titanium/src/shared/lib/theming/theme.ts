0cd44a14a35104ecfa3abc5c96a781fcbd129f80
Ansat Jartybayev <ansatjr@gmail.com>
2021-12-09 02:21:33 +0600

    chore: global styles

--- titanium/src/shared/lib/theming/theme.ts
+++ titanium/src/shared/lib/theming/theme.ts
@@ -1,36 +1,19 @@
 import {extendTheme} from "@chakra-ui/react";
 
-const styles = {};
+const styles = {
+  global: {
+    body: {
+      fontWeight: "medium",
+    },
+  },
+};
+
+const font = "Rubik, sans-serif";
 
 export const theme = extendTheme({
   styles,
   fonts: {
-    body: "Rubik, sans-serif",
-  },
-  fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "3.75rem",
-    "7xl": "4.5rem",
-    "8xl": "6rem",
-    "9xl": "8rem",
-  },
-  fontWeights: {
-    hairline: 100,
-    thin: 200,
-    light: 300,
-    normal: 400,
-    medium: 500,
-    semibold: 600,
-    bold: 700,
-    extrabold: 800,
-    black: 900,
+    body: font,
+    heading: font,
   },
 });
