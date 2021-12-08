e7af086aa7de9377c08c5646dd746099dddb068e
Ansat Jartybayev <ansatjr@gmail.com>
2021-12-09 02:20:51 +0600

    feat: users-ui

---
+++ titanium/src/features/users/ui/molecules/user-avatar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {Avatar, AvatarProps} from "@chakra-ui/react";
+
+interface UserAvatarProps extends Partial<AvatarProps> {
+  url: string;
+}
+
+export const UserAvatar: React.FC<UserAvatarProps> = ({url, ...props}) => (
+  <Avatar {...props} src={url} name="An avatar of the user" />
+);
